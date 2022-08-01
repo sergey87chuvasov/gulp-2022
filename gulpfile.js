bc0cdd0cb899b64acb0bede942d4225a6f268675
Sergey Chv <serge.techno@gmail.com>
2022-08-01 23:02:00 +0300

    added cleaning the folder with the result

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,16 @@ global.app = {
 }
 
 // import tasks
-import { copy } from "./gulp/tasks/copy.js"
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js"
+
+// file change watcher
+function watcher () {
+    gulp.watch(path.watch.files, copy);
+}
+
+// building scenarios for executing tasks
+const dev = gulp.series(reset, copy, watcher);
 
 // default script execution
-gulp.task('default', copy)
+gulp.task('default', dev)
