ebd999be8499db333a0871636e398db3def5f083
Sergey Chv <serge.techno@gmail.com>
2022-08-02 23:56:24 +0300

    added html processing

--- gulpfile.js
+++ gulpfile.js
@@ -12,15 +12,20 @@ global.app = {
 
 // import tasks
 import { copy } from "./gulp/tasks/copy.js";
-import { reset } from "./gulp/tasks/reset.js"
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 // file change watcher
 function watcher () {
     gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
 }
 
+
+const mainTasks = gulp.parallel(copy, html);
+
 // building scenarios for executing tasks
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTasks, watcher);
 
 // default script execution
-gulp.task('default', dev)
+gulp.task('default', dev);
