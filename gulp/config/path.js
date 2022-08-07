0ce5496818b1ab6bf4faac309353099d824dcef0
Sergey Chv <serge.techno@gmail.com>
2022-08-07 21:55:32 +0300

    added processing of scss files

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,15 +6,18 @@ const srcFolder = `./src`;
 
 export const path = {
     build: {
+        css: `${buildFolder}/css`,
         html: `${buildFolder}/`,
         files: `${buildFolder}/files/`
     },
     src: {
+      scss: `${srcFolder}/scss/style.scss`,
       html: `${srcFolder}/*.pug`,
       // html: `${srcFolder}/*.html`,
       files: `${srcFolder}/files/**/*.*`,
     },
     watch: {
+      scss: `${srcFolder}/scss/**/*.scss`,
       html: `${srcFolder}/**/*.pug`,
       // html: `${srcFolder}/**/*.html`,
       files: `${srcFolder}/files/**/*.*`
--- gulpfile.js
+++ gulpfile.js
@@ -19,15 +19,17 @@ import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js"
+import { scss } from "./gulp/tasks/scss.js"
 
 // file change watcher
 function watcher () {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
 }
 
 
-const mainTasks = gulp.parallel(copy, html);
+const mainTasks = gulp.parallel(copy, html, scss);
 
 // building scenarios for executing tasks
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
