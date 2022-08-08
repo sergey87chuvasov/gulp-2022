25db29ba79c74e7bb9c03ecc8a88a0b2b922ceae
Sergey Chv <serge.techno@gmail.com>
2022-08-08 16:14:08 +0300

    fixed some mistakes

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -2,6 +2,12 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
 
+import cleanCss from 'gulp-clean-css' // сжатие css файла
+import webpcss from 'gulp-webpcss' // вывод webp img
+import autoprefixer from 'gulp-autoprefixer'// добавление вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries' // группировка медиа запросов
+
+
 const sass = gulpSass(dartSass)
 
 export const scss = () => {
@@ -16,6 +22,21 @@ export const scss = () => {
     .pipe(sass({
         outputStyle: 'expanded'
     }))
+    .pipe(groupCssMediaQueries())
+    .pipe(webpcss(
+        {
+            webpClass: ".webp",
+            noWebpClass: ".no-webp"
+        }
+    ))
+    .pipe(autoprefixer({
+        grid: true,
+        overrideBrowserslist: ["last 3 versions"],
+        cascade: true
+    }))
+    // Раскоментировать если нужен не сжатый дубль файла стилей
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(cleanCss())
     .pipe(rename({
         extname: ".min.css"
     }))
