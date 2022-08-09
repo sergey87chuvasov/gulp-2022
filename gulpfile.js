7e184f2124987ca2b3cb9950840e82ad94371cd4
Sergey Chv <serge.techno@gmail.com>
2022-08-09 21:14:48 +0300

    js files processing added

--- gulpfile.js
+++ gulpfile.js
@@ -20,16 +20,18 @@ import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js"
 import { scss } from "./gulp/tasks/scss.js"
+import { js } from "./gulp/tasks/js.js"
 
 // file change watcher
 function watcher () {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
 }
 
 
-const mainTasks = gulp.parallel(copy, html, scss);
+const mainTasks = gulp.parallel(copy, html, scss, js);
 
 // building scenarios for executing tasks
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
